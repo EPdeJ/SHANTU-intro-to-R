ff814a4165c0032212f8b194b060211c1da2482b
EPdeJ <erikpieterdejong@gmail.com>
2021-12-22 13:39:29 +0800

    new setup

---
+++ codding examples.R
@@ -0,0 +1,18 @@
+
+
+
+
+
+
+
+
+# Getting help ------------------------------------------------------------
+
+# put a question mark in front of a function to get help
+
+mean(c(2,5,6,5,4))
+
+?mean # gets build-in help from the installed function "mean" 
+??mean # does a help search in all r documentation (online) 
+example(mean)
+?mean
